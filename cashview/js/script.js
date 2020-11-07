0b5df975ea11818212a9bd673bb86c49b33d137e
dnsgk4983 <dnsgk4983@gmail.com>
2020-11-07 22:54:45 +0900

    upl

--- cashview/js/script.js
+++ cashview/js/script.js
@@ -5,3 +5,63 @@ $(document).ready(function () {
 });
 
 
+$(document).ready(function () {
+    $('.man-btn').click(function(){
+        $('.man').css('border','1.5px solid #FF8200');
+        $('.man').css('color','#FF8200');
+        $('.girl').css('border','1.5px solid #B5B5B5');
+        $('.girl').css('color',' #B5B5B5');
+    });
+    $('.girl-btn').click(function(){
+        $('.girl').css('border','1.5px solid #FF8200');
+        $('.girl').css('color','#FF8200');
+        $('.man').css('border','1.5px solid #B5B5B5');
+        $('.man').css('color','#B5B5B5');
+    });
+});
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+    $('.del-btn1').click(function(){
+        $('.cat-list1').css('display','none')
+    });
+    $('.del-btn2').click(function(){
+        $('.cat-list2').css('display','none')
+    });
+    $('.del-btn3').click(function(){
+        $('.cat-list3').css('display','none')
+    });
+    $('.del-btn4').click(function(){
+        $('.cat-list4').css('display','none')
+    });
+    $('.del-btn5').click(function(){
+        $('.cat-list5').css('display','none')
+    });
+    $('.del-btn6').click(function(){
+        $('.cat-list6').css('display','none')
+    });
+    $('.del-btn7').click(function(){
+        $('.cat-list7').css('display','none')
+    });
+    $('.del-btn8').click(function(){
+        $('.cat-list8').css('display','none')
+    });
+    $('.del-btn9').click(function(){
+        $('.cat-list9').css('display','none')
+    });
+    $('.del-btn10').click(function(){
+        $('.cat-list10').css('display','none')
+    });
+    $('.del-btn11').click(function(){
+        $('.cat-list11').css('display','none')
+    });
+    $('.del-btn12').click(function(){
+        $('.cat-list12').css('display','none')
+    });
+});
